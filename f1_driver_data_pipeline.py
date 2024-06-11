e7b824b0141ce462f9277e6942225e3a9a0a7bb8
songeun moon <76909238+songeunm@users.noreply.github.com>
2024-06-11 19:59:55 +0900

    코드 포맷 정리, load_to_bigquery_task 수정

--- f1_driver_data_pipeline.py
+++ f1_driver_data_pipeline.py
@@ -3,10 +3,12 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 import requests
 import pandas as pd
+
 from airflow.providers.google.cloud.operators.gcs import GCSHook
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import (
     GCSToBigQueryOperator,
 )
+
 
 with DAG(
     dag_id="f1_driver_data_pipeline",
@@ -31,17 +33,29 @@ with DAG(
             driver_df = pd.DataFrame(driver_data)
 
             # NULL 값 처리 (FULL REFRESH)
-            driver_df['team_name'] = driver_df.groupby('full_name')['team_name'].transform(lambda x: x.ffill().bfill())
-            driver_df['team_colour'] = driver_df.groupby('team_name')['team_colour'].transform(lambda x: x.ffill().bfill())
-            driver_df['first_name'] = driver_df.groupby('full_name')['first_name'].transform(lambda x: x.ffill().bfill())
-            driver_df['last_name'] = driver_df.groupby('full_name')['last_name'].transform(lambda x: x.ffill().bfill())
-            driver_df['headshot_url'] = driver_df.groupby('full_name')['headshot_url'].transform(lambda x: x.ffill())
-            driver_df['country_code'] = driver_df.groupby('full_name')['country_code'].transform(lambda x: x.ffill().bfill())
+            driver_df["team_name"] = driver_df.groupby("full_name")[
+                "team_name"
+            ].transform(lambda x: x.ffill().bfill())
+            driver_df["team_colour"] = driver_df.groupby("team_name")[
+                "team_colour"
+            ].transform(lambda x: x.ffill().bfill())
+            driver_df["first_name"] = driver_df.groupby("full_name")[
+                "first_name"
+            ].transform(lambda x: x.ffill().bfill())
+            driver_df["last_name"] = driver_df.groupby("full_name")[
+                "last_name"
+            ].transform(lambda x: x.ffill().bfill())
+            driver_df["headshot_url"] = driver_df.groupby("full_name")[
+                "headshot_url"
+            ].transform(lambda x: x.ffill())
+            driver_df["country_code"] = driver_df.groupby("full_name")[
+                "country_code"
+            ].transform(lambda x: x.ffill().bfill())
 
             new_driver_csv = driver_df.to_csv(index=False)
 
             # GCS 업로드
-            file_path = "driver.csv" # session_key에 따라 다른 파일로 추가?
+            file_path = "driver.csv"  # session_key에 따라 다른 파일로 추가?
 
             gcs_hook = GCSHook(gcp_conn_id="google_cloud_default")
             gcs_hook.upload(
@@ -52,7 +66,7 @@ with DAG(
             )
 
             print("gcs upload complete!!!!")
-        
+
         except Exception as e:
             print(f"Error occurred: {e}")
             raise
@@ -64,11 +78,13 @@ with DAG(
     )
 
     load_to_bigquery_task = GCSToBigQueryOperator(
-        task_id = "load_to_bigquery",
-        bucket = "{{ var.value.gcs_bucket_name }}", # 버킷 이름
-        source_objects = ["driver.csv"], # 경로
-        destination_project_dataset_table = "your_project.your_dataset.your_table",
-        source_format = "CSV",
-        write_disposition = "WRITE_TRUNCATE", # 데이터를 덮어쓰도록 설정 (또는 WRITE_APPEND)
-        gcp_conn_id = "google_cloud_default", # 연결 ID 지정
+        task_id="load_to_bigquery",
+        bucket="{{ var.value.gcs_bucket_name }}",  # 버킷 이름
+        source_objects=["driver.csv"],  # 경로
+        destination_project_dataset_table="f1-data-3rd.open_f1_db.driver",
+        source_format="CSV",
+        write_disposition="WRITE_TRUNCATE",  # 데이터를 덮어쓰도록 설정 (또는 WRITE_APPEND)
+        gcp_conn_id="google_cloud_default",  # 연결 ID 지정
     )
+
+    driver_task >> load_to_bigquery_task
